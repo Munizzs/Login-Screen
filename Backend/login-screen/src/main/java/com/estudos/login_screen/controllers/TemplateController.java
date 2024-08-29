e29a6e9569025b4c096640d3b177d0c8c17cf612
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-08-29 16:44:09 -0300

    improviment: add role in HTML

--- Backend/login-screen/src/main/java/com/estudos/login_screen/controllers/TemplateController.java
+++ Backend/login-screen/src/main/java/com/estudos/login_screen/controllers/TemplateController.java
@@ -1,20 +1,27 @@
 package com.estudos.login_screen.controllers;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class TemplateController {
     @GetMapping("/home")
     public String home(Model model, Authentication authentication) {
         String username = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities  = authentication.getAuthorities();
+        List<String> roles = authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .map(role -> role.replace("ROLE_",""))
+                        .collect(Collectors.toList());
         model.addAttribute("nome", username);
+        model.addAttribute("roles", roles);
         return "home";
     }
-
-
 }
